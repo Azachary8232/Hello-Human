7597d07cfb5f8f8babcb6e2fe74d52cc099a0839
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-12-03 14:20:19 -0700

    Ninja Bonus

--- src/main/java/com/adam/hellohuman/HumanControllers.java
+++ src/main/java/com/adam/hellohuman/HumanControllers.java
@@ -1,5 +1,26 @@
 package com.adam.hellohuman;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HumanControllers {
 
+	@RequestMapping("/")
+	public String Hello(@RequestParam(value= "name", required= false) String searchquery, @RequestParam(value= "last_name", required= false) String last_name) {
+		if(searchquery == null) {
+			return "human";
+		}
+		else if(last_name == null){
+			return "human";
+		}
+		else {
+			return "Hello " + searchquery + " " + last_name; 
+		}
+	}
+	
+	
+	
+	
 }
